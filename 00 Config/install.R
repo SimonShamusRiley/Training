dfa4b3cb1f04fabc7d10cdb7f017b04ac60a4b61
Simon Shamus Riley <au802896@uni.au.dk>
2026-03-12 09:08:58 +0100

    Edit dependencies

---
+++ 00 Config/install.R
@@ -0,0 +1,13 @@
+#### Define Dependencies ####
+install.packages('tidyverse')
+install.packages('readxl')
+install.packages('writexl')
+install.packages('devtools')
+install.packages('glmmTMB')
+install.packages('car')
+install.packages('emmeans')
+install.packages('multcomp')
+install.packages('multcompView')
+
+devtools::install_github('SimonShamusRiley/powerutilities', dependencies = T, 
+                         build_vignettes = T)
